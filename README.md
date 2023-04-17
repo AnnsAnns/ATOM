430d5c5be70d61a32624166c0c7ea4ebf10c1d2c
tumGER <git@tomger.eu>
2023-04-17 19:55:59 +0200

    Minor fixes and stuff

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # *ATOM*: *A*nother *T*omato *O*riented *M*od
 
-[![Modrinth](https://img.shields.io/modrinth/game-versions/1f2HhK74?logo=modrinth&color=darkgreen&label=Download%20from%20Modrinth&style=for-the-badge)](https://modrinth.com/modpack/fabulously-optimized)
+[![Modrinth](https://img.shields.io/modrinth/game-versions/1f2HhK74?logo=modrinth&color=darkgreen&label=Download%20from%20Modrinth&style=for-the-badge)](https://modrinth.com/mod/atom-another-tomato-oriented-mod)
 
 ATOM is my excuse to learn Blockbench and at the same time add random things to Minecraft in order for me to justify wasting my time.
 
--- src/client/java/eu/tomger/AtomClient.java
+++ src/client/java/eu/tomger/AtomClient.java
@@ -4,10 +4,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class AtomClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		BlockRenderLayerMap.INSTANCE.putBlock(Atom.TOMATO_CROP_BLOCK, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(Atom.CORN_CROP_BLOCK, RenderLayer.getCutout());
 	}
